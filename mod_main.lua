9e5a0b23ed985d9368639c51a14666b3d7f1df7d
Petri Hakkinen <petrih3@gmail.com>
2019-06-11 14:33:43 +0300

    Examples of custom cards, models & textures.

--- mod_main.lua
+++ mod_main.lua
@@ -1,12 +1,14 @@
--- to avoid collisions with other mods, all assets specific to this mod are prefixed with "testmod.*"
+-- to avoid collisions with built-in game assets or other mods, all assets specific to this mod are prefixed with "testmod.*"
 
 import "testmod/particles/glitter.lua"
 
+-- define a new site on the world map
 def_site{
 	name = "testmod.test_site",
 	ui_name = "O'Mighty Site of Testing",
 }
 
+-- define a new mission
 def_mission{
 	name = "testmod.test",
 	site = "testmod.test_site",
@@ -18,21 +20,31 @@ def_mission{
 		"Subject: Welcome!\n\n" ..
 		"Welcome to modding Druidstone! We hope to see lots of cool mods in the coming months.",
 	locked = false,
+	loot = { "testmod.fork", "testmod.t_shirt", "testmod.necklace" },	-- contents of the loot box
 }
 
+-- define a new rendering material
 def_material{
-	name = "testmod.floor_material",
-	diffuse_texture = "data/textures/dungeon_tile_floor_dif.png",
+	name = "testmod.test_material",
+	diffuse_texture = "testmod/textures/centipede_yellow_dif.png",
 	specular_texture = "data/textures/dungeon_tile_floor_spec.png",
 	normal_texture = "data/textures/dungeon_tile_floor_normal.png",
 }
 
+-- define a new type of object that can be placed in the editor
 def_object{
 	name = "testmod.test_box",
 	model = "data/models/whitebox_1x1x1.fbx",
-	model_material = "testmod.floor_material",
+	model_material = "testmod.test_material",
 	particle_system = "testmod.glitter",
 	flags = F_BLOCK_MOVE + F_BLOCK_SIGHT,
+	tags = { "testmod" },
+}
+
+-- define a new type of object with custom model
+def_object{
+	name = "testmod.test_sphere",
+	model = "testmod/models/sphere.fbx",
 	tags = { "testmod" },
 }
 
@@ -43,7 +55,97 @@ def_mob{ "testmod.super_centipede",     5, 6, 8,   0,1,1,  2,5,5,  5,  4.62, 1, 
 def_aux{
 	name = "testmod.super_centipede",
 	ui_name = "Super Centipede",
-	model_material = "testmod.floor_material",
+	model_material = "testmod.test_material",
+}
+
+-- define a new ability
+def_ability{
+	name = "testmod.flip_coin",
+	ui_name = "Flip Coin",
+	ability_type = "b",
+	item_icon = 170,	-- TODO: custom icons!
+	damage = 2,
+	range = 1,
+	flags = IF_ACTION,
+	description = "An example of a new ability which flips a coin.",
+	on_use_item = function(mob, item_name, target_x, target_y)
+		if confirm_use_item(mob, item_name, "Flip It!") then
+			spend_item(mob, item_name)
+
+			if shb_random_int(1, 2) == 1 or is_power_activated2(mob, "testmod.flip_coin_lucky") then
+				add_damage_text("Heads!", mob)
+			else
+				add_damage_text("Tails!", mob)
+			end
+		end
+	end,
+	powers = { { id = "lucky", name = "Lucky", cost = 1, tooltip = "Always get heads when this power is activated." } },
+}
+
+-- define a new type of weapon
+def_weapon{
+	name = "testmod.fork",
+	ui_name = "Fork",
+	item_icon = 17,
+	weapon_type = "melee",
+	damage = 1,
+	anim_set = 1,
+	price = 50,
+	attack_sound = "swing",
+	flags = IF_LIGHT_WEAPON,
+	powers = { power_damage_bonus(1, 1) },
+}
+
+-- the object attached to wielder's hand when the weapon 'testmod.fork' is equipped
+def_object{
+	name = "testmod.fork_attachment",
+	model = "data/models/dagger.fbx",
+	model_can_emit_particles = true,
+	editor_ignore_asset = true,
+}
+
+-- define a new type of armor
+def_armor{
+	name = "testmod.t_shirt",
+	ui_name = "T-Shirt",
+	item_icon = 95,
+	armor_health_bonus = 1,
+	price = 50,
+	flags = IF_LIGHT_ARMOR,
+	powers = { power_health_bonus(2, 2) },
+}
+
+-- define a new type of accessory
+def_accessory{
+	name = "testmod.necklace",
+	ui_name = "Necklace",
+	item_icon = 9,
+	price = 50,
+	card_style = "text_box",
+	description = "A beautiful jeweled necklace.",
+}
+
+-- define a new type of consumable
+def_consumable{
+	name = "testmod.chewing_gum",
+	ui_name = "Magic Chewing Gum of Summoning",
+	item_icon = 5,
+	range = 5,
+	description = "Summon a Slug.",
+	on_use_item = function(mob, item_name, target_x, target_y)
+		local map = mob.map
+
+		if target_x == nil then
+			target_x, target_y = choose_target(mob, item_name, TARGET_LOS_FROM_MOB + TARGET_UNBLOCKED_FOR_SUMMON)
+			if target_x == nil then return end
+		end
+
+		spend_item(mob, item_name)
+		spawn("slug", map, target_x, target_y, -1)
+		remove_map_markers(map)
+
+		return true
+	end,
 }
 
 -- make alterations to the world map
--- test.lua
+++ test.lua
@@ -1,7 +1,8 @@
 lever_pulled = false
 
 function on_init_map()
-	print_msg("init map")
+	-- add a temporary ability for the first hero
+	set_inventory(party[1].inventory, "testmod.flip_coin", 1)
 end
 
 function on_toggle_lever(mob, lever)
@@ -12,4 +13,5 @@ function on_draw_objectives(map, ctx)
 	local status
 	if lever_pulled then status = true end
 	ctx.objective("pull_lever", "Pull the Lever", status, "P")
+	ctx.bonus_objective_loot_box()
 end
